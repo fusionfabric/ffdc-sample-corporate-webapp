adaad710a90e8a9689784bfe11d9cee6799e1c0f
David <david.bocle@finastra.com>
2020-06-04 16:23:03 +0200

    feat(pwa): Add PWA capabilities

--- apps/client/src/app/app-init/app-init.module.ts
+++ apps/client/src/app/app-init/app-init.module.ts
@@ -2,9 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, APP_INITIALIZER } from '@angular/core';
 
 import { AppInitRoutingModule } from './app-init.routing.module';
-import { AuthService, AuthModule } from '@ffdc-corporate-banking-sample/ui/auth';
+import {
+  AuthService,
+  AuthModule,
+} from '@ffdc-corporate-banking-sample/ui/auth';
 import { AppInitComponent } from './app-init.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [AppInitComponent],
@@ -13,6 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppInitRoutingModule,
     AuthModule,
     BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [
     {
