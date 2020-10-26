f9c232ec1aa7af285cd28617abd2f6b88a7431bb
frigui dorsaf <dorsaf.frigui@finastra.com>
2020-10-26 15:53:40 +0100

    set transaction list by page

--- libs/ui/cdk/statement/src/lib/statement.component.ts
+++ libs/ui/cdk/statement/src/lib/statement.component.ts
@@ -4,7 +4,7 @@ import {
   OnInit,
 } from '@angular/core';
 import { AccountStatement } from '@ffdc/api_corporate-accounts/interfaces';
-import {from} from 'rxjs';
+import { from } from 'rxjs';
 import { groupBy, mergeMap, reduce, toArray } from 'rxjs/operators'
 
 @Component({
@@ -13,33 +13,41 @@ import { groupBy, mergeMap, reduce, toArray } from 'rxjs/operators'
   styleUrls: ['./statement.component.scss'],
 })
 export class StatementComponent implements OnInit {
-  @Input() transactions: AccountStatement[];
 
-  transactionsDate:any[];
-  constructor() {}
+  private _transactions!: AccountStatement[];
+  transactionsByDate: any[];
 
-  ngOnInit(): void {
 
-    from(this.transactions).pipe(
+  @Input()
+  public get transactions(): AccountStatement[] {
+    return this._transactions
+ 
+  }
+
+  public set transactions(value: AccountStatement[]) {
+    this._transactions= value;
+    from(this._transactions).pipe(
       groupBy(item => item.postingDate),
       mergeMap((group$) => group$.pipe(reduce((acc, cur) => [...acc, cur], []))),
       toArray()
-   )
- 
-
-  .subscribe(x => {
-    this.transactionsDate=x;
-    }
-    );
-}
-
-getAmountColor(item) {
-  let color = '';
-  if (item.transactionType === 'CREDIT') {
-    color = 'var(--uxg-grass-100)';
+    )
+      .subscribe(x => {
+        this.transactionsByDate = x;
+      });
   }
-  return color;
-}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getAmountColor(item) {
+    let color = '';
+    if (item.transactionType === 'CREDIT') {
+      color = 'var(--uxg-grass-100)';
+    }
+    return color;
+  }
 }
 
 // Skeleton
@@ -49,4 +57,4 @@ getAmountColor(item) {
   templateUrl: './statement.skeleton.html',
   styleUrls: ['./statement.component.scss'],
 })
-export class StatementSkeletonComponent {}
+export class StatementSkeletonComponent { }
