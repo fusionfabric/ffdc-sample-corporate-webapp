98d479632829de6b258216c5b738bc4e2569e9a1
dorsaf frigui <dorsafrigui@gmail.com>
2021-04-26 15:09:16 +0200

    update dependencies (#15)

--- libs/ui/shared/src/lib/fds.module.ts
+++ libs/ui/shared/src/lib/fds.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { GlobalNavModule } from '@ffdc/uxg-angular-components/global-nav';
-import { SkeletonTextModule } from '@ffdc/uxg-angular-components/skeleton-text';
-import { UxgUserProfileMenuModule } from '@ffdc/uxg-angular-components/user-profile-menu';
-import { AccountCardModule } from '@ffdc/uxg-angular-components/cards/account-card';
-import { UxgTableModule } from '@ffdc/uxg-angular-components/table';
+import { GlobalNavModule } from '@finastra/angular-components/global-nav';
+import { SkeletonTextModule } from '@finastra/angular-components/skeleton-text';
+import { UxgUserProfileMenuModule } from '@finastra/angular-components/user-profile-menu';
+import { AccountCardModule } from '@finastra/angular-components/cards/account-card';
+import { UxgTableModule } from '@finastra/angular-components/table';
 
 @NgModule({
   exports: [
