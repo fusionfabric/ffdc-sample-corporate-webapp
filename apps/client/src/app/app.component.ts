5bc8bfdd40fa7c543b36cbd527f5e15e24560450
frigui dorsaf <dorsafrigui@gmail.com>
2022-02-21 16:11:00 +0100

    improvements

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component, ElementRef, Inject, PLATFORM_ID, ViewChild } from '@angular/core';
 import { AuthService } from '@ffdc-corporate-banking-sample/ui/auth';
-import { routes } from './constants';
-import { Router } from '@angular/router';
-import { isPlatformBrowser} from '@angular/common';
+import "@finastra/app-bar";
+import "@finastra/sidenav";
+import "@finastra/user-profile";
+import "@finastra/button";
+import "@finastra/app-card";
+import "@material/mwc-icon-button";
+import "@material/mwc-drawer";
 
 @Component({
   selector: 'fcbs-root',
@@ -12,33 +16,14 @@ import { isPlatformBrowser} from '@angular/common';
 export class AppComponent {
   appName = 'Account Services';
 
-  navigationNodes = routes;
-
   isBrowser: boolean;
 
   @ViewChild("appBar") appBar: ElementRef;
   @ViewChild("userProfile") userProfile: ElementRef;
   
 
-  constructor(public auth: AuthService, private router: Router,@Inject(PLATFORM_ID) private platformId: Object) {
-    this.isBrowser = isPlatformBrowser(this.platformId);
-    if(this.isBrowser) {
-      import("@finastra/app-bar");
-      import("@finastra/user-profile");
-      import("@finastra/button");
-      import("@finastra/sidenav");
-      import("@finastra/app-card");
-      import("@material/mwc-icon-button");
-      import("@material/mwc-drawer");
-    }
-  }
+  constructor(public auth: AuthService) {
 
-  nodeChosen(node) {
-    this.router.navigate([node.path]);
-  }
-
-  brandAction() {
-    this.router.navigate(['home']);
   }
 
   logout() {
@@ -46,7 +31,6 @@ export class AppComponent {
   }
 
   ngAfterViewInit() {
-    console.log(this.userProfile.nativeElement);
     this.userProfile.nativeElement.setAttribute("username",this.auth.user$.value['username']);
     this.appBar.nativeElement.setAttribute("appname", this.appName);  
   }
